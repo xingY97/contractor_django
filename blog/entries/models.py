5e43993535ced6e8d92f84466640a0ee8980d86d
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-03-06 15:17:46 -0800

    created entry table for databse

---
+++ blog/entries/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+class Entry(models.Model):
+    entry_title = models.CharField(max_length=50)
+    entry_text = models.TextField()
+    entry_date = models.DateTimeField(auto_now_add=True)
+    entry_author = models.ForeignKey(User, on_delete=Models.CASCADE)
