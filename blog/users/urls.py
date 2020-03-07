3686e45ccf6fe63c89fce4af2d067c9c08f4c3ed
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-03-06 17:38:50 -0800

    users are able to log out, and then with a link to log back in

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name = 'register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'login')
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name = 'logout')
 ]
