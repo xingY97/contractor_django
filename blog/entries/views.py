eadf37614aa3983fb68bed80b0f13a21b5f5df18
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-03-06 17:12:35 -0800

    added paginate_by, so each pages u=only showing 3 posts

--- blog/entries/views.py
+++ blog/entries/views.py
@@ -7,6 +7,7 @@ class HomeView(ListView):
     template_name = 'entries/index.html'
     context_object_name = "blog_entries"
     ordering = ['-entry_date']
+    paginate_by = 3
 
 class EntryView(DetailView):
     model = Entry
