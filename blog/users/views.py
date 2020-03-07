1e0fa14bde545a5c6c1c6c86677ef626dcf2d593
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-03-06 17:48:24 -0800

    user gets redirect to home page

--- blog/users/views.py
+++ blog/users/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login
 
 def register(request):
     if request.method =='POST':
         form = RegistrationForm(request.Post)
         if form.is_valid():
             form.save()
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request,user)
+            return redirect ('blog-home')
     else:
         form = RegistrationForm()
 
