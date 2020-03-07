5a6ef60c3e23227199b2f4b38bc6e9e33aa3cba6
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-03-06 17:11:13 -0800

    fixed bug on create post form, added feature to view posts in a timely oreder

--- blog/entries/urls.py
+++ blog/entries/urls.py
@@ -4,6 +4,6 @@ from .views import HomeView, EntryView, CreateEntryView
 urlpatterns = [
     path('', HomeView.as_view(), name = 'blog-home'),
     path('entry/<int:pk>/', EntryView.as_view(), name = 'entry-detail'),
-    path('create_entry/', CreateEntryView.as_view(), name = "create_entry"),
+    path('create_entry/', CreateEntryView.as_view(success_url='/'), name = "create_entry"),
     
 ]
--- blog/entries/views.py
+++ blog/entries/views.py
@@ -6,6 +6,7 @@ class HomeView(ListView):
     model = Entry
     template_name = 'entries/index.html'
     context_object_name = "blog_entries"
+    ordering = ['-entry_date']
 
 class EntryView(DetailView):
     model = Entry
@@ -15,3 +16,7 @@ class CreateEntryView(CreateView):
     model = Entry
     template_name = 'entries/create_entry.html'
     fields = ['entry_title', 'entry_text']
+
+    def form_valid(self,form):
+        form.instance.entry_author = self.request.user
+        return super().form_valid(form)
