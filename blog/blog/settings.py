1e98ed6729f076f2f5c2a4bcdc25d261a2fb1b7b
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-03-06 18:22:06 -0800

    idk why bootstrap isnt connecting to the html files

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '25mi5whc&0fvoq2l0ld)z25jyz*e2t2%9j9x38ky#&^&*_x@4l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'uniqueprojectname.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'uniqueprojectname.herokuapp.com']
 
 
 # Application definition
